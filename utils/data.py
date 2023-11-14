c52813aabfdaa8cea5acd74ad74bd1e8b4de2552
pictureken <spetoken@outlook.jp>
2023-11-14 14:54:15 +0900

    fi

--- utils/data.py
+++ utils/data.py
@@ -121,8 +121,8 @@ class GenerateGridDataset(Dataset):
         x = np.linspace(0, 1, 1000)
         y = np.linspace(0, 1, 1000)
         xv, yv = np.meshgrid(x, y)
-        xv = xv.flatten()
-        yv = yv.flatten()
+        xv = xv.flatten().reshape(len(xv), 1)
+        yv = yv.flatten().reshape(len(yv), 1)
         self.samples = np.concatenate((xv, yv), axis=1)
 
     def __getitem__(self, index: int) -> torch.Tensor:
