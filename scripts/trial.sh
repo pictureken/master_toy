4e081df86391d3957ad6c2092eb69d63a0d5ae1c
pictureken <spetoken@outlook.jp>
2023-11-15 15:16:44 +0900

    add scripts

--- scripts/trial.sh
+++ scripts/trial.sh
@@ -1,9 +1,9 @@
-trial_list=($(seq 1 1 1000d))
+trial_list=($(seq 1 1 1000))
 
 for trial in "${trial_list[@]}"
 do
     python train.py \
-    --hidden_size 100 \
-    --epoch 1 \
+    --hidden_size 10000 \
+    --epoch 1000 \
     --trial ${trial}
 done
